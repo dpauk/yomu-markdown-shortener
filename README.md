c289ddb451de7544b18719458e6099e0de71566b
David Albone <dpauk@users.noreply.github.com>
2022-07-09 14:49:49 +0100

    Added splitting

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Takes the output from a [Yomu](https://www.yomu-reader.com/) markdown-formatted 
 
 ## Usage
 
-1. Add the output from Yomu into `input.md`
+1. Add the output from Yomu into `yomu_markdown_input.md`
 2. Run `python yms.py`
-3. The formatted output will appear in `output.md`
+3. The formatted output will appear in `output\output.md`
+
+### Optional
+
+To split the file into individual chapter in the output directory: `python yms.py -s` or `python yms.py --split`
--- yms.py
+++ yms.py
@@ -1,23 +1,77 @@
-output = open('output.md', 'w')
+import argparse
+import os
 
-passed_intro = False
+INPUT_FILE = 'yomu_markdown_input.md'
+OUTPUT_DIR = os.path.join(os.getcwd(), 'output')
 
-with open('input.md', 'r') as input:
-    for line in input:
-        if line == '## Annotations\n':
-            passed_intro = True
-            output.write(f'{line}\n')
-            continue
-        
-        if not passed_intro:
-            output.write(line)
-            continue
-        
-        if line.startswith('###'):
-            output.write(f'{line}\n')
-            continue
+def split():
+    passed_intro = False
+    output = open(os.path.join(OUTPUT_DIR, 'title.md'), 'w')
+    chapter_counter = 0
 
-        if line.startswith('>'):
-            output.write(f'{line}\n')
-        
-output.close()
+    with open(INPUT_FILE, 'r') as input:
+        for line in input:
+
+            ## Find the end of the intros
+            if line == '## Annotations\n':
+                passed_intro = True
+                chapter_counter += 1
+                continue
+            
+            # Output into title
+            if not passed_intro:
+                output.write(line)
+                continue
+            
+            # New chapter
+            if line.startswith('###'):
+                output.close()
+                output = open(os.path.join(OUTPUT_DIR, f'{chapter_counter} - {line[3:]}.md'), 'w')
+                output.write(f'{line}\n')
+                continue
+
+            # A quote line to output
+            if line.startswith('>'):
+                output.write(f'{line}\n')
+    
+    output.close()
+
+def parse():
+    output = open(os.path.join(OUTPUT_DIR, 'output.md'), 'w')
+
+    passed_intro = False
+
+    with open(INPUT_FILE, 'r') as input:
+        for line in input:
+            ## Find the end of the intros
+            if line == '## Annotations\n':
+                passed_intro = True
+                output.write(f'{line}\n')
+                continue
+            
+            # Output into title
+            if not passed_intro:
+                output.write(line)
+                continue
+            
+            # New chapter
+            if line.startswith('###'):
+                output.write(f'{line}\n')
+                continue
+
+            # A quote line to output
+            if line.startswith('>'):
+                output.write(f'{line}\n')
+    
+    output.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process Yomu reader markdown files')
+    parser.add_argument('-s', '--split', help='Split file based on chapter titles', action='store_true')
+    args = parser.parse_args()
+
+    if args.split:
+        split()
+    else:
+        parse()
